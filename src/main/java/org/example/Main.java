858540d61cc6288cc1b6277c02e94c370a9a6ab9
Amar-Pratap-Singh <AmarPratap.Singh@iiitb.ac.in>
2023-11-06 18:07:59 +0530

    Updated pom.xml and other files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,68 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+
 public class Main {
+
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    
+    public static int add(int a, int b){
+        return a + b;
+    } 
+
+    public static int subtract(int a, int b){
+        return a - b;
+    } 
+
+    public static int multiply(int a, int b){
+        return a * b;
+    } 
+
+    public static int divide(int a, int b){
+        return a / b;
+    } 
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("----Welcome to Calculator----");
+        System.out.println("Choose Your Operation");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        // System.out.println("5. Exit");
+
+        int choice = sc.nextInt();
+
+        System.out.println("Enter First Number: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter Second Number: ");
+        int b = sc.nextInt();
+
+        // take input
+        if (choice == 1){
+            add(a, b);
+        }
+        else if (choice == 2){
+            subtract(a, b);
+        }
+        else if (choice == 3){
+            multiply(a, b);
+        }  
+        else if (choice == 4){
+            divide(a, b);
+        }
+        else{
+            System.out.println("Invalid Input!");
+        }
     }
 }
